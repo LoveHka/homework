b34592b886ac18a99410e896c43327eb7cf8d78a
LoveH <56201413+LoveHka@users.noreply.github.com>
2025-12-05 18:34:10 +0300

    Create second.py

---
+++ Комментарии/second.py
@@ -0,0 +1,28 @@
+students = ["Аня", "Боря", "Вова", "Галя"]
+
+print(students)
+
+students.append("Дима")
+print(students)
+
+students.insert(2, "Женя")
+print(students)
+
+students.pop(2)
+print(students)
+
+print(students[1])
+
+print(len(students))
+
+for s in students:
+    print()
+
+if "Вова" in students:
+    print("Вова найден")
+
+students.sort()
+print(students)
+
+students.reverse()
+print(students)
